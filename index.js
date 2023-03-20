b51e0f9caec67c90c258b3bc70a1ddcbf790108c
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-20 15:15:35 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+
+const companyName = 'Scuber'
+mostProfitableNeighborhood = 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea'
+companyCeo = 'Susan Smith'
+let companyCeo = 'Susan Smith'
